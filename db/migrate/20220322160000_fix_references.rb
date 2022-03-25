5ef389a96edfca190b0ce948714f1051c6182915
goddraw <coolo_ricky@hotmail.com>
2022-03-25 14:20:50 -0400

    removed remove columns

--- db/migrate/20220322160000_fix_references.rb
+++ db/migrate/20220322160000_fix_references.rb
@@ -1,12 +1,12 @@
 class FixReferences < ActiveRecord::Migration[5.2]
   def change
     # adresses table
-    #remove_column :adresses, :customers_id
+    ########remove_column :adresses, :customers_id
 
     # batteries table
-    remove_column :batteries, :buildings_id
+    #######remove_column :batteries, :buildings_id
     add_reference :batteries, :building, foreign_key: true
-    remove_column :batteries, :employeeId
+    #######remove_column :batteries, :employeeId
     add_reference :batteries, :employee
 
     # building_details table
@@ -16,31 +16,31 @@ class FixReferences < ActiveRecord::Migration[5.2]
     # drop_table :buildingdetails
 
     # buildings table
-    remove_column :buildings, :customers_id
-    remove_column :buildings, :adresses_id
+    ######remove_column :buildings, :customers_id
+    ######remove_column :buildings, :adresses_id
     add_reference :buildings, :customer, foreign_key: true
     add_reference :buildings, :adress, foreign_key: true
-    remove_column :buildings, :Adress_of_the_building
+    ######remove_column :buildings, :Adress_of_the_building
 
     # columns table
     add_reference :columns, :battery, foreign_key: true
 
     # customers table
-    remove_column :customers, :users_id
+    ######remove_column :customers, :users_id
     add_reference :customers, :user, foreign_key: true
-    remove_column :customers, :Company_headquarters_adress
-    remove_column :customers, :city
+    ######remove_column :customers, :Company_headquarters_adress
+    ######remove_column :customers, :city
     add_reference :customers, :adress, foreign_key: true
 
     # detailsbuildings table
-    remove_column :detailsbuildings, :buildings_id
+    ######remove_column :detailsbuildings, :buildings_id
     add_reference :detailsbuildings, :building, foreign_key: true
 
     # elevators table
     add_reference :elevators, :column, foreign_key: true
 
     # employees table
-    remove_column :employees, :users_id
+    ######remove_column :employees, :users_id
     add_reference :employees, :user, foreign_key: true    
 
   end
